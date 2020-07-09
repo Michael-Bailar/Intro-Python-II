3e8c58ad84ef7272b1c1b19f95e65863d4ef195d
Michael-B1764 <mikebailar@gmail.com>
2020-07-09 16:14:33 -0700

    met mpv

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,34 @@
 from room import Room
 from player import Player
+from item import Item
+
+# Declare Items
+item = {
+    "dagger": Item("dagger", "Short, but to the point"),
+    "buckler": Item("buckler", "Can block some attacks")
+}
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",["buckler"]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
+
 
 
 # Link rooms together
@@ -33,6 +41,78 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+
+# set player and start location
+
+player = Player(room['outside'], [])
+
+# Declare defaults
+playing = True
+action = ''
+local_items = Room(player.current_room.name, player.current_room.description, player.current_room.items).items
+
+# main interpreter
+
+while playing is True:
+    print(f"========== {player.current_room.name} ==========")
+    print(player.current_room.description)
+    if player.current_room.items:
+        print(f"There are items here.")
+        print(local_items)
+        print("     (take/drop [item])")
+    print("what do you do?")
+    action = input("-")
+    print("================================================")
+
+    if (action == 'quit'):
+        print("Thanks for playing!")
+        playing = False
+    elif (action == 'help'):
+        print("Choose a direction(n,s,e,w)\n take [item], drop [item]\n view inventory (i), or quit")
+    elif (action == 'i'):
+        print("--Inventory--")
+        print(player.items)
+    elif (action == 'n'):
+        print("Proceeding north...")
+        try:
+            player.current_room = player.current_room.n_to
+        except AttributeError:
+            print("The way is blocked...")
+    elif (action == 's'):
+        print("Proceeding south...")
+        try:
+            player.current_room = player.current_room.s_to
+        except AttributeError:
+            print("The way is blocked...")
+    elif (action == 'e'):
+        print("Proceeding east...")
+        try:
+            player.current_room = player.current_room.e_to
+        except AttributeError:
+            print("The way is blocked...")
+    elif (action == 'w'):
+        print("Proceeding west...")
+        try:
+            player.current_room = player.current_room.w_to
+        except AttributeError:
+            print("The way is blocked...")
+
+    for item in local_items:
+        if action == f"take {item}":
+            print(f"You got {item}")
+            player.take_item(item)
+
+    for item in player.items:
+        if action == f"drop {item}":
+            print(f"\n You lost the {item}")
+            player.drop_item(item)
+
+
+print("================================================")
+
+
+
+
 
 #
 # Main
@@ -50,43 +130,3 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-
-player = Player(room['outside'])
-action = ''
-
-while action != 'q':
-    current_room = player.current_room
-    print(f"========== {current_room.name} ==========")
-    print(player.current_room.description)
-    print("what do you do?")
-    action = input("-")
-    print("================================================")
-    if (action == 'q'):
-        print("Thanks for playing!")
-        playing = 'q'
-    elif (action == 'n'):
-        print("Proceeding north...")
-        try:
-            player = Player(current_room.n_to)
-        except AttributeError:
-            print("The way is blocked...")
-    elif (action == 's'):
-        print("Proceeding south...")
-        try:
-            player = Player(current_room.s_to)
-        except AttributeError:
-            print("The way is blocked...")
-    elif (action == 'e'):
-        print("Proceeding east...")
-        try:
-            player = Player(current_room.e_to)
-        except AttributeError:
-            print("The way is blocked...")
-    elif (action == 'w'):
-        print("Proceeding west...")
-        try:
-            player = Player(current_room.w_to)
-        except AttributeError:
-            print("The way is blocked...")
-
-print("================================================")
--- src/player.py
+++ src/player.py
@@ -1,6 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item import Item
 
 class Player:
-    def __init__(self, current_room):
+    def __init__(self, current_room, items):
         self.current_room = current_room
+        self.items = items
+
+    def take_item(self, item):
+        self.current_room.items.remove(item)
+        self.items.append(item)
+
+    def drop_item(self, item):
+        self.current_room.items.append(item)
+        self.items.remove(item)
